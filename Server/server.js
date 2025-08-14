08f5818c68fa24e5aab516cf70d09e31e27324ae
Finch <168956586+sajjalf23@users.noreply.github.com>
2025-08-14 19:34:21 +0500

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -22,14 +22,17 @@ app.use( cookieParser());
 
 app.use(cors({
   origin: function(origin, callback) {
+    // allow requests with no origin like mobile apps or Postman
     if (!origin) return callback(null, true);
     if (allowedOrigins.includes(origin)) {
-      callback(null, true);
+      return callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      return callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 
