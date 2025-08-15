06f6fbd20f51224ace2c68008f5383d86a907910
Finch <168956586+sajjalf23@users.noreply.github.com>
2025-08-15 15:49:25 +0500

    Update Resetpassword.jsx

--- client/src/Pages/Resetpassword.jsx
+++ client/src/Pages/Resetpassword.jsx
@@ -7,6 +7,7 @@ import { toast } from "react-toastify";
 import axios from "axios";
 
 const Resetpassword = () => {
+  axios.defaults.withCredentials = true;
   const { BackendUrl } = useContext(AppContext);
   const navigate = useNavigate();
 
