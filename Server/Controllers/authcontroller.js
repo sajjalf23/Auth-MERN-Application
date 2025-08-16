74a2bd7c2c314d4e8b3ac5a674d1dc9a6d9e60cb
Finch <168956586+sajjalf23@users.noreply.github.com>
2025-08-16 10:28:31 +0500

    Update authcontroller.js

--- Server/Controllers/authcontroller.js
+++ Server/Controllers/authcontroller.js
@@ -30,13 +30,23 @@ export const register = async (req, res) => {
             expiresIn: '7d'
         });
 
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-            path: "/",
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        });
+        // res.cookie('token', token, {
+        //     httpOnly: true,
+        //     secure: process.env.NODE_ENV === 'production',
+        //     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        //     path: "/",
+        //     maxAge: 7 * 24 * 60 * 60 * 1000
+        // });
+
+           res.cookie('token', token, {
+               httpOnly: true,
+               secure: true,
+               sameSite: 'none',
+               domain: '.vercel.app', // <- allows subdomains
+               path: '/',
+               maxAge: 7*24*60*60*1000
+           });
+
 
         const mailOptions = {
             from: process.env.SENDER_EMAIL,
@@ -90,13 +100,22 @@ export const login = async (req, res) => {
             expiresIn: "7d",
         });
 
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-            path: "/",
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-        });
+        // res.cookie("token", token, {
+        //     httpOnly: true,
+        //     secure: process.env.NODE_ENV === "production",
+        //     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        //     path: "/",
+        //     maxAge: 7 * 24 * 60 * 60 * 1000,
+        // });
+
+        res.cookie('token', token, {
+               httpOnly: true,
+               secure: true,
+               sameSite: 'none',
+               domain: '.vercel.app', // <- allows subdomains
+               path: '/',
+               maxAge: 7*24*60*60*1000
+           });
 
         return res.status(200).json({
             success: true,
